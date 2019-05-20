a7a90938747543bf0c59a50d6cda58909cf57112
YEZIHANGISM <beok02089163@163.com>
2019-05-20 21:48:53 +0800

    personal web

--- ismblog/urls.py
+++ ismblog/urls.py
@@ -9,18 +9,14 @@ urlpatterns = [
     path("user/<int:pk>", views.UserDetailView.as_view(), name="user-detail"),
     path("user/<int:pk>/update", views.UserUpdate.as_view(), name="user-update"),
 
-    # path("blogs/", views.BlogListView.as_view(), name="blogs"),
     path("blog/<int:pk>", views.BlogDetailView.as_view(), name="blog-detail"),
     path("blog/<int:pk>/delete", views.BlogDelete.as_view(), name="blog-delete"),
     path("blog/<int:pk>/update", views.BlogUpdate.as_view(), name="blog-update"),
 
-    # path("createblog/", views.BlogCreate.as_view(), name='blog-create'),
     path("createblog/", views.create_blog, name="blog-create"),
     path("comment/<int:pk>/create", views.create_comment, name="comment-create"),
 
-    # path("comment/<int:pk>/create", views.CommentCreate.as_view(), name="comment-create"),
-
-    path("register/", views.create_user, name="register"),
+    # path("register/", views.create_user, name="register"),
 
     path("search/", views.search, name="search"),
 ]
